266238c3510e4839995d701848294004862beb67
andrew arkwright <andrew.arkwright@hotmail.com>
2022-09-29 07:39:48 -0400

    Make some adjustments to fix some route and post problems with createCharacter

--- routes/character.js
+++ routes/character.js
@@ -4,7 +4,7 @@ const characterController = require('../controllers/character')
 const { ensureAuth } = require('../middleware/auth')
 console.log("CharRoute")
 
-router.get('/', ensureAuth, characterController.getCharacters) //why not /character?
+router.get('/', ensureAuth, characterController.getCharacters) //We use / and not /character because we are already in /character at the moment when this is used
 
 router.post('/createCharacter', characterController.createCharacter)
 
