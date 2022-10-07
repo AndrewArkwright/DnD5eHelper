5216ee5c17cf6f9ce00613325b7770e0e44f4ae4
andrew arkwright <andrew.arkwright@hotmail.com>
2022-10-07 17:39:30 -0400

    Adjust controller, Mongoose schema, and viewCharacters.ejs so they send all the information to the database and can load0 it on the page. Make some gramatical changes to viewCharacters.ejs.

--- controllers/viewCharacters.js
+++ controllers/viewCharacters.js
@@ -3,7 +3,8 @@ const char = require('../models/character')
 module.exports = {
     getCharacters: async (request, response) => {
         try{
-            const characterList = await char.find() //char is from the first line in this document since Model should be the only thing contacting the DB, gets stuck here
+            console.log(request.user.id)
+            const characterList = await char.find({User: request.user.id}) //char is from the first line in this document since Model should be the only thing contacting the DB, gets stuck here
             response.render('viewCharacters.ejs', {data: characterList})
         }catch(err){
             console.log(err)
--- models/character.js
+++ models/character.js
@@ -13,7 +13,101 @@ const characterSchema = new mongoose.Schema({
     Class: {
       type: String,
       required: true,
+    },
+    Name: {
+      type: String,
+      required: true,
+    },
+    Alignment: {
+      type: String,
+      required: true,
+    },
+    raceTraits: {
+      type: String,
+      required: true
+    },
+    classTraits: {
+      type: String,
+      required: true
+    },
+    abilityScoreArray: {
+      type: Array,
+      required: true
+    },
+    abilityScoreMod: {
+      type: Array,
+      required: true
+    },
+    abilityScoresConst: {
+      type: Array,
+      required: true
+    },
+    hp: {
+      type: Number,
+      required: true
+    },
+    speed: {
+      type: Number,
+      required: true
+    },
+    profBonus: {
+      type: Number,
+      required: true
+    },
+    savingThrows: {
+      type: Array,
+      required: true
+    },
+    weaponProf: {
+      type: Array,
+      required: true
+    },
+    armorProf: {
+      type: Array,
+      required: true
+    },
+    language: {
+      type: Array,
+      required: true
+    },
+    toolProf: {
+      type: Array,
+      required: true
+    },
+    equipment: {
+      type: Array,
+      required: true
+    },
+    Traits: {
+      type: String,
+      required: true
+    },
+    Ideals: {
+      type: String,
+      required: true
+    },
+    Bonds: {
+      type: String,
+      required: true
+    },
+    Flaws: {
+      type: String,
+      required: true
+    },
+    Description: {
+      type: String,
+      required: true
+    },
+    User: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     }
 })
   
 module.exports = mongoose.model('char', characterSchema)
+
+/*
+
+            await char.create({Race: request.body.Race, Subrace: request.body.Subrace, Class: request.body.Class, raceTrait: raceTrait, classTrait: classTrait, abilityScoreArray: abilityScoreArray, abilityScoreMod: abilityScoreMod, abilityScoresConst: abilityScoresConst, hp: hp, speed: speed, proficiencyBonus: profBonus, savingThrows: savingThrows, weaponProf: weaponProf, armorProf: armorProf, language: language, toolProf: toolProf, equipment: equipment, Traits: request.user.Traits, Ideals: request.user.Ideals, Bonds: request.user.Bonds, Flaws: request.user.Flaws, Description: request.user.Description, User: request.user.id})
+
+*/
